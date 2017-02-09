f283b3959e5f9f68500032ada5ab27838a52e9e9
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2017-02-09 13:11:02 -0300

    test fixtures

--- test/ogu_lang/parser_test.clj
+++ test/ogu_lang/parser_test.clj
@@ -4,15 +4,18 @@
 
 (def euler1 "test/euler/e1.ogu")
 (def euler2 "test/euler/e2.ogu")
+(def euler6 "test/euler/e6.ogu")
+(def euler8 "test/euler/e8.ogu")
+
 
 (defn eval-mod [name]
-      (parse-module {:eval true} name))
+  (parse-module {:eval true} name))
 
 (deftest test-1
-         (testing "Check Euler 1"
-                  (is (= 233168 (eval-mod euler1)))))
+  (testing "Check Euler"
+    (is (= 233168 (eval-mod euler1)))
+    (is (= 4613732 (eval-mod euler2)))
+    (is (= 25164150 (eval-mod euler6)))
+    (is (= 23514624000 (eval-mod euler8)))))
 
 
-(deftest test-2
-         (testing "Check Euler 2"
-                  (is (= 4613732 (eval-mod euler2)))))
