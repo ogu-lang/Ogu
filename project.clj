b55115807d7ec8d9b08c4f497f7c2b5a161a5a68
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2017-11-02 11:39:53 -0300

    change core match version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  [com.github.lalyos/jfiglet "0.0.8"]
                  [instaparse "1.4.8"]
                  [rhizome "0.2.7"]])
