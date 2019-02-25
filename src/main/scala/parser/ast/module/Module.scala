1c2cc20c5f65cb7265235cbe46eef6022479846b
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-02-24 23:49:28 -0300

    remove duplicated code

--- src/main/scala/parser/ast/module/Module.scala
+++ src/main/scala/parser/ast/module/Module.scala
@@ -554,20 +554,6 @@ object Module  {
     }
   }
 
-  def funcCallEndToken(tokens:TokenStream) : Boolean = {
-    if (tokens.isEmpty) {
-      true
-    } else {
-      tokens.nextToken().exists { next =>
-        next == NL || next.isInstanceOf[PIPE_OPER] || next.isInstanceOf[OPER] || next.isInstanceOf[DECL] ||
-          next == INDENT || next == DEDENT || next == ASSIGN  ||
-          next == DOLLAR || next == COMMA || next == LET || next == VAR || next == DO || next == THEN ||
-          next == ELSE || next == RPAREN || next == IN || next == RBRACKET || next == RCURLY || next == WHERE
-      }
-    }
-
-  }
-
   def parseAtom(tokens:TokenStream) : Expression = {
     val atom = tokens.consume(classOf[ATOM])
     Atom(atom.value)
