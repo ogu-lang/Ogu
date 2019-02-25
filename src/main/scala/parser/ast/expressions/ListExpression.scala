c54165d086f484af7bb0230d7791130b651ab10a
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-02-25 19:40:13 -0300

    simplify ListExpression

--- src/main/scala/parser/ast/expressions/ListExpression.scala
+++ src/main/scala/parser/ast/expressions/ListExpression.scala
@@ -4,6 +4,8 @@ import lexer._
 import parser.Expression
 import parser.ast._
 
+import scala.annotation.tailrec
+
 trait ValidRangeExpression extends Expression
 case class RangeExpression(rangeInit:Expression, rangeEnd:Expression) extends ValidRangeExpression
 case class RangeExpressionUntil(rangeInit:Expression, rangeEnd:Expression) extends ValidRangeExpression
@@ -24,58 +26,56 @@ case class ListExpression(expressions: List[Expression], guards: Option[List[Lis
 object ListExpression extends ExpressionParser {
 
   override def parse(tokens: TokenStream): Expression = {
-      tokens.consume(LBRACKET)
-      if (tokens.peek(RBRACKET)) {
-        tokens.consume(RBRACKET)
-        EmptyListExpresion()
-      }
-      else {
-        val exprs = parseListOfCommaSeparatedExpressions(tokens)
-        var expr = if (exprs.length == 1) exprs.head else ListExpression(exprs, None)
-
-        val finalExpr = if (tokens.peek(DOTDOT)) {
-          tokens.consume(DOTDOT)
-          parseEndRange(tokens, expr, include=true)
-        }
-        else if (tokens.peek(DOTDOTLESS)) {
-          tokens.consume(DOTDOTLESS)
-          parseEndRange(tokens, expr, include = false)
-        }
-        else if (tokens.peek(DOTDOTDOT)) {
-          tokens.consume(DOTDOTDOT)
-          InfiniteRangeExpression(expr)
-        }
-        else if (tokens.peek(GUARD)) {
-          tokens.consume(GUARD)
-          var listOfGuards = List.empty[ListGuard]
-          var guard = parseListGuard(tokens)
-          listOfGuards = guard :: listOfGuards
-          while (tokens.peek(COMMA)) {
-            tokens.consume(COMMA)
-            guard = parseListGuard(tokens)
-            listOfGuards = guard :: listOfGuards
-          }
-          ListExpression(List(expr), Some(listOfGuards.reverse))
-        } else {
-          expr
-        }
-
-        tokens.consume(RBRACKET)
-        if (!finalExpr.isInstanceOf[ValidRangeExpression])
-          ListExpression(List(finalExpr), None)
-        else
-          finalExpr
-      }
+    tokens.consume(LBRACKET)
+    if (tokens.peek(RBRACKET)) {
+      tokens.consume(RBRACKET)
+      EmptyListExpresion()
     }
+    else {
+      val exprs = parseListOfCommaSeparatedExpressions(tokens)
+      val expr = if (exprs.length == 1) exprs.head else ListExpression(exprs, None)
+      val finalExpr = if (tokens.peek(DOTDOT)) {
+        tokens.consume(DOTDOT)
+        parseEndRange(tokens, expr, include = true)
+      }
+      else if (tokens.peek(DOTDOTLESS)) {
+        tokens.consume(DOTDOTLESS)
+        parseEndRange(tokens, expr, include = false)
+      }
+      else if (tokens.peek(DOTDOTDOT)) {
+        tokens.consume(DOTDOTDOT)
+        InfiniteRangeExpression(expr)
+      }
+      else if (tokens.peek(GUARD)) {
+        tokens.consume(GUARD)
+        ListExpression(List(expr), Some(consumeListGuards(tokens, List(parseListGuard(tokens)))))
+      } else {
+        expr
+      }
+      tokens.consume(RBRACKET)
+      if (!finalExpr.isInstanceOf[ValidRangeExpression])
+        ListExpression(List(finalExpr), None)
+      else
+        finalExpr
+    }
+  }
 
+  @tailrec
+  private[this] def consumeListGuards(tokens: TokenStream, guards: List[ListGuard]): List[ListGuard] = {
+    if (!tokens.peek(COMMA)) {
+      guards.reverse
+    } else {
+      tokens.consume(COMMA)
+      consumeListGuards(tokens, parseListGuard(tokens) :: guards)
+    }
+  }
 
-
-  private[this] def parseEndRange(tokens: TokenStream, expr: Expression, include: Boolean) : Expression = {
-    expr match {
-      case expression: ListExpression if expression.expressions.size == 2 =>
-        val lexpr = expression.expressions
-        val rangeInit = lexpr.head
-        val rangeIncrement = SubstractExpression(List(lexpr.tail.head, lexpr.head))
+  private[this] def parseEndRange(tokens: TokenStream, expression: Expression, include: Boolean): Expression = {
+    expression match {
+      case ListExpression(exprs, _) if exprs.size == 2 =>
+        val rangeInit = exprs(0)
+        val rangeEnd = exprs(1)
+        val rangeIncrement = SubstractExpression(List(rangeEnd, rangeInit))
         if (include)
           RangeWithIncrementExpression(rangeInit, rangeIncrement, LogicalExpression.parse(tokens))
         else
@@ -88,7 +88,7 @@ object ListExpression extends ExpressionParser {
     }
   }
 
-  def parseListGuard(tokens:TokenStream) : ListGuard = {
+  def parseListGuard(tokens: TokenStream): ListGuard = {
     if (tokens.peek(LPAREN) && tokens.peek(2, classOf[ID]) && tokens.peek(3, COMMA)) {
       tokens.consume(LPAREN)
       val listOfIds = consumeListOfIdsSepByComma(tokens)
