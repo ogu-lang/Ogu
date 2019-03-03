1c575628a30a6e3e023b954122445ed246d6f063
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-03-03 16:12:16 -0300

    optimize DeclGen

--- src/main/scala/codegen/clojure/decls/DeclGen.scala
+++ src/main/scala/codegen/clojure/decls/DeclGen.scala
@@ -1,25 +1,25 @@
 package codegen.clojure.decls
 
-import codegen.{CodeGenerator, Translator}
 import codegen.clojure.expressions.ExpressionsGen._
+import codegen.{CodeGenerator, Translator}
 import parser.ast.decls._
-import parser.ast.expressions.{Expression, Identifier}
 import parser.ast.expressions.list_ops.ConsExpression
-import parser.ast.expressions.literals.{Atom, LiteralExpression}
+import parser.ast.expressions.literals.Atom
 import parser.ast.expressions.types._
+import parser.ast.expressions.{Expression, Identifier}
+import scala.annotation.tailrec
 
 object DeclGen {
+
+  type StrList = List[String]
 
   implicit object DispatchDeclTranslator extends Translator[DispatchDecl] {
 
     override def mkString(node: DispatchDecl): String = {
-      val strBuf = new StringBuilder()
-      strBuf ++= s"(defmulti ${node.id} "
-      node.dispatcher match {
-        case ClassDispatcher => strBuf ++= "class)\n"
-        case ExpressionDispatcher(expr) => strBuf ++= s"${CodeGenerator.buildString(expr)})\n"
-      }
-      strBuf.mkString
+      s"(defmulti ${node.id} " + (node.dispatcher match {
+        case ClassDispatcher => "class)\n"
+        case ExpressionDispatcher(expr) => s"${CodeGenerator.buildString(expr)})\n"
+      })
     }
   }
 
@@ -44,7 +44,6 @@ object DeclGen {
           s"{[${args.init.map(CodeGenerator.buildString(_)).mkString(" ")} & ${CodeGenerator.buildString(args.last)}] $a}"
 
         case DefArg(expression) => s"${CodeGenerator.buildString(expression)}"
-
       }
     }
 
@@ -79,7 +78,7 @@ object DeclGen {
           strBuf ++= s"(defmethod $id ${matches.map(toClojureDefMatchArg).mkString(" ")} "
           strBuf ++= s"[${args.map(CodeGenerator.buildString(_)).mkString(" ")}]\n"
           val whereDefs = whereBlock.whereDefs
-          strBuf ++= s"\t(let [${whereDefs.map(WhereDefTranslator.mkStringAsLet).mkString("\n")}]"
+          strBuf ++= s"\t(let [${whereDefs.map(mkStringAsLet).mkString("\n")}]"
           strBuf ++= s"\n\t\t${CodeGenerator.buildString(body)}))"
 
       }
@@ -122,32 +121,12 @@ object DeclGen {
           s"(def $id  \n" +
             s"(cond ${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")}))"
         case WhereDefTupled(idList, None, body) =>
-          var strBuf = new StringBuilder()
-          strBuf ++= s"(def _*temp*_ ${CodeGenerator.buildString(body)})\n"
-          var i = 0
-          for (id <- idList) {
-            strBuf ++= s"(def $id (nth _*temp*_ $i))\n"
-            i += 1
-          }
-          strBuf.toString()
+          s"(def _*temp*_ ${CodeGenerator.buildString(body)})\n" +
+            idList.zipWithIndex.map { case (id, i) => s"(def $id (nth _*temp*_ $i))" }.mkString("\n")
       }
     }
 
-    def mkStringAsLet(node: WhereDef): String = {
-      node match {
-        case WhereDefSimple(id, None, body) => s"$id ${CodeGenerator.buildString(body)}"
-        case WhereDefSimple(id, Some(args), body) =>
-          s"$id (fn [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}] ${CodeGenerator.buildString(body)})"
-        case WhereDefWithGuards(id, Some(args), guards) =>
-          s"$id (fn [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}] \n" +
-            s"(cond ${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")}))"
-        case WhereDefWithGuards(id, None, guards) =>
-          s"$id  \n" +
-            s"(cond ${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")})"
-        case WhereDefTupled(idList, None, body) =>
-          s"[${idList.mkString(" ")}] ${CodeGenerator.buildString(body)}"
-      }
-    }
+
   }
 
   implicit object WhereBlockTranslator extends Translator[WhereBlock] {
@@ -162,34 +141,24 @@ object DeclGen {
 
     override def mkString(node: SimpleDefDecl): String = {
       if (node.isMulti()) {
-        // special case, a single multimethod
         MultiDefDeclTranslator.mkString(MultiDefDecl(node.id, List(node)))
       }
       else {
+        def prefix(inner: Boolean): String = if (inner) "(defn- " else "(defn "
+
+        def sArgs(args: List[DefArg]) = args.map(a => CodeGenerator.buildString(a)).mkString(" ")
+
         node match {
           case SimpleDefDecl(inner, id, args, BodyGuardsExpresion(guards), None) =>
             val conds = s"\t(cond\n${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")})"
-            if (inner) {
-              s"(defn- $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n\t$conds)"
-            } else {
-              s"(defn $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n\t$conds)"
-            }
+            s"${prefix(inner)} $id [${sArgs(args)}]\n\t$conds)"
 
           case SimpleDefDecl(inner, id, args, body, None) =>
-            if (inner) {
-              s"(defn- $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n\t\t${CodeGenerator.buildString(body)})\n\n"
-            } else {
-              s"(defn $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n\t\t${CodeGenerator.buildString(body)})\n\n"
-            }
+            s"${prefix(inner)} $id [${sArgs(args)}]\n\t\t${CodeGenerator.buildString(body)})\n\n"
 
           case SimpleDefDecl(inner, id, args, body, Some(whereBlock)) =>
-            if (inner) {
-              s"(defn- $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n" +
-                s"\t\t${CodeGenerator.buildString(whereBlock)}\n    ${CodeGenerator.buildString(body)})\n\n"
-            } else {
-              s"(defn $id [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}]\n" +
-                s"\t\t${CodeGenerator.buildString(whereBlock)}\n    ${CodeGenerator.buildString(body)})\n\n"
-            }
+            s"${prefix(inner)} $id [${sArgs(args)}]\n" +
+              s"\t\t${CodeGenerator.buildString(whereBlock)}\n\t${CodeGenerator.buildString(body)})\n\n"
         }
       }
     }
@@ -199,171 +168,98 @@ object DeclGen {
   implicit object MultiDefDeclTranslator extends Translator[MultiDefDecl] {
 
     override def mkString(node: MultiDefDecl): String = {
-      val strBuf = new StringBuilder()
       if (!node.patternMatching()) {
-        strBuf ++= s"\n(defn ${node.id}\n"
-        for (decl <- node.decls) {
-          strBuf ++= "([" + decl.args.map(arg => s"${CodeGenerator.buildString(arg.expression)}").mkString(" ") + "] "
-          decl.whereBlock match {
-            case None =>
-              strBuf ++= s"${CodeGenerator.buildString(decl.body)})\n"
-            case Some(whereBlock) =>
-              val whereDefs = whereBlock.whereDefs
-              strBuf ++= s"(let [${whereDefs.map(WhereDefTranslator.mkStringAsLet).mkString("\n")}]"
-              strBuf ++= s"\n\t${CodeGenerator.buildString(decl.body)})"
-          }
-        }
-        strBuf ++= ")\n\n"
+        s"\n(defn ${node.id}\n" +
+          node.decls.map { decl =>
+            "([" + decl.args.map(arg => s"${CodeGenerator.buildString(arg.expression)}").mkString(" ") + "] " +
+              (decl.whereBlock match {
+                case None => s"${CodeGenerator.buildString(decl.body)})\n"
+                case Some(WhereBlock(whereDefs)) =>
+                  s"(let [${whereDefs.map(mkStringAsLet).mkString("\n")}]" +
+                    s"\n\t${CodeGenerator.buildString(decl.body)})"
+              })
+          }.mkString("") + ")\n\n"
       }
       else {
-        val args: List[String] = 0.until(node.count).map(i => s"arg_$i").toList
-        strBuf ++= s"\n(defn ${node.id} [" + args.mkString(" ") + "]\n"
-        if (node.count == node.args.length) {
-          strBuf ++= "(let [" + node.args.zip(args).map(p => s"${p._1} ${p._2}").mkString(" ") + "]\n"
-        } else if (node.count < node.args.length) {
-          val args_args = args ++ args
-          strBuf ++= "(let [" + node.args.zip(args_args).map(p => s"${p._1} ${p._2}").mkString(" ") + "]\n"
-        }
-        strBuf ++= "\t(cond\n"
-        for (decl <- node.decls) {
-          var andList = List.empty[String]
-          var letDecls = List.empty[String]
-          var argDecls = decl.args
-          var namedArgs = args
-          if (decl.whereBlock.nonEmpty) {
-            val whereDefs = decl.whereBlock.get.whereDefs
-            letDecls = whereDefs.map(wd => s"${whereDefAsLet(wd)}")
-          }
-          while (argDecls.nonEmpty) {
-            val arg = argDecls.head
-            arg match {
-              case DefArg(Identifier(id)) if args.contains(id) =>
-              // nothing
-              case DefArg(EmptyListExpresion) =>
-                andList = s"\t\t(empty? ${namedArgs.head})" :: andList
-
-              case DefArg(ConsExpression(args)) =>
-                val tail = args.last
-                val head = args.init
-                letDecls = s"[${head.map(CodeGenerator.buildString(_)).mkString(" ")} & ${CodeGenerator.buildString(tail)}] ${namedArgs.head}" :: letDecls
-
-              case DefArg(ListExpression(defArgs, None)) =>
-                letDecls = s"[${defArgs.map(CodeGenerator.buildString(_)).mkString(" ")}] ${namedArgs.head}]" :: letDecls
-
-              case DefArg(ConstructorExpression(cls, ctorArgs)) =>
-                andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
-                var argDecls = List.empty[String]
-                for (arg <- ctorArgs) {
-                  arg match {
-                    case Identifier(id) => argDecls = s"$id (.$id ${namedArgs.head})" :: argDecls
-                  }
-                }
-                letDecls = argDecls.reverse ++ letDecls
-
-              case DefArg(RecordConstructorExpression(cls, ctorArgs)) =>
-                andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
-                var argDecls = List.empty[String]
-                for (arg <- ctorArgs) {
-                  arg match {
-                    case Identifier(id) => argDecls = s"$id (.$id ${namedArgs.head})" :: argDecls
-                  }
-                }
-                letDecls = argDecls.reverse ++ letDecls
-              case DefArg(IdIsType(_, cls)) =>
-                andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
-
-              case DefArg(VariadicArg(expr)) =>
-                andList = s"\t\t(= ${namedArgs.head} $expr)" :: andList
-
-              case DefArg(TupleExpression(List(lit: Expression, Identifier("_")))) =>
-                lit match {
-                  case Identifier(n) =>
-                    andList = s":else " :: andList
-                    letDecls = s"[$n & _] ${namedArgs.head}" :: letDecls
-                  case _ =>
-                    andList = s"\t\t(= (head ${namedArgs.head}) ${CodeGenerator.buildString(lit)})" :: andList
-                }
-
-              case DefArg(exp: Expression) =>
-                andList = s"\t\t(= ${namedArgs.head} ${CodeGenerator.buildString(exp)})" :: andList
-
+        val namedArgs: List[String] = 0.until(node.count).map(i => s"_*_arg_$i").toList
+        val args2 = namedArgs ++ namedArgs
+        s"\n(defn ${node.id} [" + namedArgs.mkString(" ") + "]\n" +
+          "(let [" + node.args.zip(args2).map { case (k, v) => s"$k $v" }.mkString(" ") + "]\n\t(cond\n" +
+          node.decls.map { decl =>
+            val letDecls = decl.whereBlock match {
+              case None => Nil
+              case Some(WhereBlock(whereDefs)) => whereDefs.map(mkStringAsLet)
             }
-            argDecls = argDecls.tail
-            namedArgs = namedArgs.tail
-          }
-          strBuf ++= mergeAndLets(andList, letDecls, decl.body)
-        }
-        strBuf ++= ")))\n\n"
+            buildCond(decl.body, decl.args, namedArgs, Nil, letDecls)
+          }.mkString("") + ")))\n\n"
       }
-      strBuf.mkString
     }
 
-    /*
+    @tailrec
     private[this]
-    def buildAndLets(args: List[String], argDecls: List[DefArg], namedArgs: List[String], andList: List[String], letDecls: List[String]): (List[String], List[String]) = {
+    def buildCond(body:Expression, argDecls: List[DefArg], args: StrList, andList: StrList, lets: StrList): String = {
       argDecls.headOption match {
-        case None => (andList, letDecls)
+        case None => mergeAndLets(andList, lets, body)
         case Some(arg) =>
           arg match {
-            case DefArg(Identifier(id)) if args.contains(id) => // nothing
-            case DefArg(_: EmptyListExpresion) =>
-              andList = s"\t\t(empty? ${namedArgs.head})" :: andList
+            case DefArg(EmptyListExpresion) =>
+              val and = s"\t\t(empty? ${args.head})"
+              buildCond(body, argDecls.tail, args.tail, and :: andList, lets)
 
-            case DefArg(ConsExpression(args)) =>
-              val tail = args.last
-              val head = args.init
-              letDecls = s"[${head.map(CodeGenerator.buildString(_)).mkString(" ")} & ${CodeGenerator.buildString(tail)}] ${namedArgs.head}" :: letDecls
+            case DefArg(ConsExpression(cArgs)) =>
+              val tail = CodeGenerator.buildString(cArgs.last)
+              val head = cArgs.init.map(CodeGenerator.buildString(_)).mkString(" ")
+              val letDecl = s"[$head & $tail] ${args.head}"
+              buildCond(body, argDecls.tail, args.tail, andList, letDecl :: lets)
 
             case DefArg(ListExpression(defArgs, None)) =>
-              letDecls = s"[${defArgs.map(CodeGenerator.buildString(_)).mkString(" ")}] ${namedArgs.head}]" :: letDecls
+              val letDecl = s"[${defArgs.map(CodeGenerator.buildString(_)).mkString(" ")}] ${args.head}]"
+              buildCond(body, argDecls.tail, args.tail, andList, letDecl :: lets)
 
             case DefArg(ConstructorExpression(cls, ctorArgs)) =>
-              andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
-              var argDecls = List.empty[String]
-              for (arg <- ctorArgs) {
-                arg match {
-                  case Identifier(id) => argDecls = s"$id (.$id ${namedArgs.head})" :: argDecls
-                }
+              val and = s"(isa-type? $cls ${args.head})"
+              val ctorDecls = ctorArgs.flatMap {
+                case Identifier(id) => Some(s"$id (.$id ${args.head})")
+                case _ => None
               }
-              letDecls = argDecls.reverse ++ letDecls
+              buildCond(body, argDecls.tail, args.tail, and :: andList, ctorDecls ++ lets)
 
             case DefArg(RecordConstructorExpression(cls, ctorArgs)) =>
-              andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
-              var argDecls = List.empty[String]
-              for (arg <- ctorArgs) {
-                arg match {
-                  case Identifier(id) => argDecls = s"$id (.$id ${namedArgs.head})" :: argDecls
-                }
+              val and = s"(isa-type? $cls ${args.head})"
+              val ctorDecls = ctorArgs.flatMap {
+                case Identifier(id) => Some(s"$id (.$id ${args.head})")
+                case _ => None
               }
-              letDecls = argDecls.reverse ++ letDecls
+              buildCond(body, argDecls.tail, args.tail, and :: andList, ctorDecls ++ lets)
+
             case DefArg(IdIsType(_, cls)) =>
-              andList = s"(isa-type? $cls ${namedArgs.head})" :: andList
+              val and = s"(isa-type? $cls ${args.head})"
+              buildCond(body, argDecls.tail, args.tail, and :: andList, lets)
 
             case DefArg(VariadicArg(expr)) =>
-              andList = s"\t\t(= ${namedArgs.head} $expr)" :: andList
+              val and = s"\t\t(= ${args.head} $expr)"
+              buildCond(body, argDecls.tail, args.tail, and :: andList, lets)
 
             case DefArg(TupleExpression(List(lit: Expression, Identifier("_")))) =>
               lit match {
                 case Identifier(n) =>
-                  andList = s":else " :: andList
-                  letDecls = s"[$n & _] ${namedArgs.head}" :: letDecls
+                  val newAndList = s":else " :: andList
+                  val newLetDecls = s"[$n & _] ${args.head}" :: lets
+                  buildCond(body, argDecls.tail, args.tail, newAndList, newLetDecls)
+
                 case _ =>
-                  andList = s"\t\t(= (head ${namedArgs.head}) ${CodeGenerator.buildString(lit)})" :: andList
+                  val newAndList = s"\t\t(= (head ${args.head}) ${CodeGenerator.buildString(lit)})" :: andList
+                  buildCond(body, argDecls.tail, args.tail, newAndList, lets)
               }
 
             case DefArg(exp: Expression) =>
-              andList = s"\t\t(= ${namedArgs.head} ${CodeGenerator.buildString(exp)})" :: andList
-
+              val newAndList = s"\t\t(= ${args.head} ${CodeGenerator.buildString(exp)})" :: andList
+              buildCond(body, argDecls.tail, args.tail, newAndList, lets)
           }
-          argDecls = argDecls.tail
-          namedArgs = namedArgs.tail
       }
-
     }
-  }
-    */
 
-    private[this] def mergeAndLets(andList: List[String], letDecls: List[String], body: Expression) : String = {
+    private[this] def mergeAndLets(andList: StrList, letDecls: StrList, body: Expression) : String = {
       val sBody = CodeGenerator.buildString(body)
       val sLet = letDecls match {
         case Nil => s"$sBody"
@@ -376,21 +272,7 @@ object DeclGen {
       }
     }
 
-    private[this] def whereDefAsLet(whereDef: WhereDef): String = {
-      whereDef match {
-        case WhereDefSimple(id, None, body) => s"$id ${CodeGenerator.buildString(body)}"
-        case WhereDefSimple(id, Some(args), body) =>
-          s"$id (fn [${args.map(CodeGenerator.buildString(_)).mkString(" ")}] ${CodeGenerator.buildString(body)})"
-        case WhereDefWithGuards(id, Some(args), guards) =>
-          s"$id (fn [${args.map(CodeGenerator.buildString(_)).mkString(" ")}] \n" +
-            s"(cond ${guards.map(CodeGenerator.buildString(_)).mkString("\n")}))"
-        case WhereDefTupled(idList, None, body) =>
-          val stemp = s"_*temp*_ ${CodeGenerator.buildString(body)}\n"
-          val indices = 0.until(idList.length)
-          val sind = idList.zip(indices).map{case (id,i) => s"$id (nth _*temp*_ $i)"}.mkString("\n")
-          stemp + sind
-      }
-    }
+
   }
 
   implicit object DefDeclTranslator extends Translator[DefDecl] {
@@ -401,6 +283,22 @@ object DeclGen {
         case md: MultiDefDecl => CodeGenerator.buildString(md)
         case mm: MultiMethod => CodeGenerator.buildString(mm)
       }
+    }
+  }
+
+  private[this] def mkStringAsLet(node: WhereDef): String = {
+    node match {
+      case WhereDefSimple(id, None, body) => s"$id ${CodeGenerator.buildString(body)}"
+      case WhereDefSimple(id, Some(args), body) =>
+        s"$id (fn [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}] ${CodeGenerator.buildString(body)})"
+      case WhereDefWithGuards(id, Some(args), guards) =>
+        s"$id (fn [${args.map(a => CodeGenerator.buildString(a)).mkString(" ")}] \n" +
+          s"(cond ${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")}))"
+      case WhereDefWithGuards(id, None, guards) =>
+        s"$id  \n" +
+          s"(cond ${guards.map(g => CodeGenerator.buildString(g)).mkString("\n")})"
+      case WhereDefTupled(idList, None, body) =>
+        s"[${idList.mkString(" ")}] ${CodeGenerator.buildString(body)}"
     }
   }
 
