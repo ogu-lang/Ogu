3eedd68c18a1f96eca61c174185e8033d1f45dfe
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-03-05 15:24:43 -0300

    remove var from ExpressionsGen

--- src/main/scala/codegen/clojure/expressions/ExpressionsGen.scala
+++ src/main/scala/codegen/clojure/expressions/ExpressionsGen.scala
@@ -1,18 +1,18 @@
 package codegen.clojure.expressions
 
-import codegen.{CodeGenerator, Translator}
 import codegen.clojure.expressions.ArithmeticsGen._
 import codegen.clojure.expressions.ComparativeGen._
 import codegen.clojure.expressions.ControlGen._
 import codegen.clojure.expressions.DeclsExprGen._
 import codegen.clojure.expressions.FunctionsGen._
-import codegen.clojure.expressions.LiteralsGen._
 import codegen.clojure.expressions.ListOpsGen._
+import codegen.clojure.expressions.LiteralsGen._
 import codegen.clojure.expressions.LogicalGen._
 import codegen.clojure.expressions.PartialOperGen._
 import codegen.clojure.expressions.RangeGen._
 import codegen.clojure.expressions.RegexpGen._
 import codegen.clojure.expressions.TypeExprGen._
+import codegen.{CodeGenerator, Translator}
 import parser.ast.decls.BodyGuardsExpresion
 import parser.ast.expressions._
 import parser.ast.expressions.arithmetic.PartialOper
@@ -29,6 +29,29 @@ import parser.ast.expressions.vars.{BindDeclExpression, LetDeclExpression, Using
 
 object ExpressionsGen {
 
+  implicit object IdentifierTranslator extends Translator[Identifier] {
+
+    override def mkString(node: Identifier): String = {
+      val id = node.name
+      val prefix = if (VarDeclExpressionTranslator.isVariable(id)) "@" else ""
+      val pos = id.lastIndexOf('.')
+      if (pos < 1) {
+        prefix + id
+      }
+      else {
+        val parts = id.split('.')
+        parts match {
+          case _ if parts.exists(p => p.headOption.exists(c => c.isUpper)) =>
+            val sb = new StringBuilder(id)
+            sb.replace(pos, pos + 1, "/")
+            prefix + sb.toString()
+          case Array(head, last) => s"(.${parts.last} ${parts.head})"
+          case _ => prefix + id
+        }
+      }
+    }
+  }
+
   implicit object ExpressionTranslator extends Translator[Expression] {
 
     override def mkString(node: Expression): String = {
@@ -42,35 +65,7 @@ object ExpressionsGen {
             case List(e) => mkString(e)
             case _ =>  s"(do ${expressions.map(mkString).mkString("\n")})"
           }
-        case Identifier(id) =>
-          val strBuf = new StringBuilder()
-          if (VarDeclExpressionTranslator.isVariable(id)) {
-            strBuf ++= "@"
-          }
-          val pos = id.lastIndexOf('.')
-          if (pos < 1) {
-            strBuf ++= id
-          } else {
-            var containsClass = false
-            val parts = id.split('.')
-            for (p <- parts) {
-              if (p.head.isUpper) {
-                containsClass = true
-              }
-            }
-            if (containsClass) {
-              val sb = new StringBuilder(id)
-              sb.replace(pos, pos + 1, "/")
-              strBuf ++= sb.toString()
-            }
-            else if (parts.length == 2) {
-              strBuf ++= s"(.${parts.last} ${parts.head})"
-            }
-            else {
-              strBuf ++= id
-            }
-          }
-          strBuf.mkString
+        case i:Identifier => CodeGenerator.buildString(i)
         case StringLiteral(value) => value
         case ae: ArithmeticExpression => CodeGenerator.buildString(ae)
         case bd: BindDeclExpression => CodeGenerator.buildString(bd)
@@ -95,7 +90,6 @@ object ExpressionsGen {
         case _ => s"EXPRESSION(${node.getClass})"
       }
     }
-
   }
 
   implicit object TopLevelExpressionTranslator extends Translator[TopLevelExpression] {
